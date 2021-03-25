489fa90f8d45eef814d01e0f86ff381a269b55d6
Melissa20XX <65194138+Melissa20XX@users.noreply.github.com>
2021-03-24 18:40:08 -0600

    Update Connect4.cs

---
+++ Connect4.cs
@@ -0,0 +1,99 @@
+/*
+
+    ####################################
+    #                                  #
+    # SODV1201 Connect 4 Final Project #
+    #                                  #
+    #  Created by: Kirsten Kristensen  #
+    #          and Melissa Wiebe       #
+    #                                  #
+    ####################################
+
+
+Notes:
+    
+    The easiest way to check for a victory would probably be to check for a vertical,
+    horizontal or diagonal victory for each piece the moment its placed.
+
+    
+    Here's an example:
+    
+    We're playing as X, this is the board so far.
+
+    |   |   |   |   |   |   |   |
+    |   |   |   |   |   |   |   |
+    |   |   |   |   |   | X |   |
+    |   | 0 |   | X |   | 0 |   |
+    |   | 0 | 0 | X | X | 0 | 0 |
+    | X | 0 | X | 0 | 0 | X | X |
+    -----------------------------
+    0 went first, and has taken 9 turns.
+
+
+    Now it's our 9th turn.
+    We're about to win, so we drop in an X
+
+                      X
+                      .
+    |   |   |   |   | . |   |   |
+    |   |   |   |   | . |   |   |
+    |   |   |   |   | . | X |   |
+    |   |   |   | X | . | 0 |   |
+    |   | 0 | 0 | X | X | 0 | 0 |
+    | X | 0 | X | 0 | 0 | X | X |
+    -----------------------------    
+
+        
+    First, we check vertically. If the cell below also contains an 'X', we add 1 to a tally var and repeat
+    If we come across an empty or non-matching square, we stop counting in that direction, reset the tally to 0, and begin counting in a different direction.
+
+    |   |   |   |   | . |   |   |
+    |   |   |   |   | . |   |   |
+    |   |   |   |   | . |   |   |
+    |   |   |   |   | X |   |   |
+    |   |   |   |   | 1 |   |   |
+    |   |   |   |   | # |   |   |
+    -----------------------------
+    There is not a vertical victory, the tally is 1
+    
+    Next, we need to check for a left-diagonal, horizontal, and right-diagonal victory.
+    For the sake of consistency, we'll check the left side before the right side for all 3
+
+    '#' is a cell we check that doesn't increment the tally
+
+    |   |   |   |   | . |   |   |
+    |   |   |   |   | . |   |   |
+    |   |   |   | # | . |   |   |   
+    |   |   |   |   | X |   |   |
+    |   |   |   |   |   | # |   |
+    |   |   |   |   |   |   |   |
+    -----------------------------
+    There is not a left diagonal victory, the tally is 0
+
+
+    |   |   |   |   | . |   |   |
+    |   |   |   |   | . |   |   |
+    |   |   |   |   | . |   |   |
+    |   |   | # | 1 | X | # |   |
+    |   |   |   |   |   |   |   |
+    |   |   |   |   |   |   |   |
+    -----------------------------
+    There is not a horizontal victory, the tally is 0
+
+    
+
+    |   |   |   |   | . |   |   |
+    |   |   |   |   | . |   |   |
+    |   |   |   |   | . | 1 |   |
+    |   |   |   |   | X |   |   |
+    |   |   |   | 1 |   |   |   |
+    |   |   | 2 |   |   |   |   |
+    -----------------------------
+    Since the total tally in this direction is 3,
+    our victory is confirmed 
+
+    
+    
+
+
+*/
