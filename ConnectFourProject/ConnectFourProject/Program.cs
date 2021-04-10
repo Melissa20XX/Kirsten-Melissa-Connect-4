9e1487d685c32226ef1136efafb3e7e3744c6e1d
kirstenk12 <63880768+kirstenk12@users.noreply.github.com>
2021-04-10 14:49:19 -0600

    Moved  Notes to the bottom

--- ConnectFourProject/ConnectFourProject/Program.cs
+++ ConnectFourProject/ConnectFourProject/Program.cs
@@ -9,89 +9,6 @@
     #                                  #
     ####################################
 
-
-Notes:
-    
-    The easiest way to check for a victory would probably be to check for a vertical,
-    horizontal or diagonal victory for each piece the moment its placed.
-
-    
-    Here's an example:
-    
-    We're playing as X, this is the board so far.
-
-    |   |   |   |   |   |   |   |
-    |   |   |   |   |   |   |   |
-    |   |   |   |   |   | X |   |
-    |   | 0 |   | X |   | 0 |   |
-    |   | 0 | 0 | X | X | 0 | 0 |
-    | X | 0 | X | 0 | 0 | X | X |
-    -----------------------------
-    0 went first, and has taken 9 turns.
-
-
-    Now it's our 9th turn.
-    We're about to win, so we drop in an X
-
-                      X
-                      .
-    |   |   |   |   | . |   |   |
-    |   |   |   |   | . |   |   |
-    |   |   |   |   | . | X |   |
-    |   |   |   | X | . | 0 |   |
-    |   | 0 | 0 | X | X | 0 | 0 |
-    | X | 0 | X | 0 | 0 | X | X |
-    -----------------------------    
-
-        
-    First, we check vertically. If the cell below also contains an 'X', we add 1 to a tally var and repeat
-    If we come across an empty or non-matching square, we stop counting in that direction, reset the tally to 0, and begin counting in a different direction.
-
-    |   |   |   |   | . |   |   |
-    |   |   |   |   | . |   |   |
-    |   |   |   |   | . |   |   |
-    |   |   |   |   | X |   |   |
-    |   |   |   |   | 1 |   |   |
-    |   |   |   |   | # |   |   |
-    -----------------------------
-    There is not a vertical victory, the tally is 1
-    
-    Next, we need to check for a left-diagonal, horizontal, and right-diagonal victory.
-    For the sake of consistency, we'll check the left side before the right side for all 3
-
-    '#' is a cell we check that doesn't increment the tally
-
-    |   |   |   |   | . |   |   |
-    |   |   |   |   | . |   |   |
-    |   |   |   | # | . |   |   |   
-    |   |   |   |   | X |   |   |
-    |   |   |   |   |   | # |   |
-    |   |   |   |   |   |   |   |
-    -----------------------------
-    There is not a left diagonal victory, the tally is 0
-
-
-    |   |   |   |   | . |   |   |
-    |   |   |   |   | . |   |   |
-    |   |   |   |   | . |   |   |
-    |   |   | # | 1 | X | # |   |
-    |   |   |   |   |   |   |   |
-    |   |   |   |   |   |   |   |
-    -----------------------------
-    There is not a horizontal victory, the tally is 0
-
-    
-
-    |   |   |   |   | . |   |   |
-    |   |   |   |   | . |   |   |
-    |   |   |   |   | . | 1 |   |
-    |   |   |   |   | X |   |   |
-    |   |   |   | 1 |   |   |   |
-    |   |   | 2 |   |   |   |   |
-    -----------------------------
-    Since the total tally in this direction is 3,
-    our victory is confirmed 
-
 */
 
 using System;
@@ -457,7 +374,7 @@ namespace ConnectFour
                 {
                     Console.WriteLine("Please enter your name: ");
                     string PlayerName = Console.ReadLine();
-                    Console.WriteLine(PlayerName + "You will be playing as 'X'");
+                    Console.WriteLine(PlayerName + " You will be playing as 'X'");
                     Human Player = new Human('X', PlayerName);
                     Computer Comp = new Computer('O');
 
@@ -502,3 +419,85 @@ namespace ConnectFour
         }
     }
 }
+
+//Notes:
+
+//The easiest way to check for a victory would probably be to check for a vertical,
+//horizontal or diagonal victory for each piece the moment its placed.
+
+    
+//    Here's an example:
+    
+//    We're playing as X, this is the board so far.
+
+//    |   |   |   |   |   |   |   |
+//    |   |   |   |   |   |   |   |
+//    |   |   |   |   |   | X |   |
+//    |   | 0 |   | X |   | 0 |   |
+//    |   | 0 | 0 | X | X | 0 | 0 |
+//    | X | 0 | X | 0 | 0 | X | X |
+//    -----------------------------
+//    0 went first, and has taken 9 turns.
+
+
+//    Now it's our 9th turn.
+//    We're about to win, so we drop in an X
+
+//                      X
+//                      .
+//    |   |   |   |   | . |   |   |
+//    |   |   |   |   | . |   |   |
+//    |   |   |   |   | . | X |   |
+//    |   |   |   | X | . | 0 |   |
+//    |   | 0 | 0 | X | X | 0 | 0 |
+//    | X | 0 | X | 0 | 0 | X | X |
+//    -----------------------------    
+
+        
+//    First, we check vertically. If the cell below also contains an 'X', we add 1 to a tally var and repeat
+//    If we come across an empty or non-matching square, we stop counting in that direction, reset the tally to 0, and begin counting in a different direction.
+
+//    |   |   |   |   | . |   |   |
+//    |   |   |   |   | . |   |   |
+//    |   |   |   |   | . |   |   |
+//    |   |   |   |   | X |   |   |
+//    |   |   |   |   | 1 |   |   |
+//    |   |   |   |   | # |   |   |
+//    -----------------------------
+//    There is not a vertical victory, the tally is 1
+    
+//    Next, we need to check for a left-diagonal, horizontal, and right-diagonal victory.
+//    For the sake of consistency, we'll check the left side before the right side for all 3
+
+//    '#' is a cell we check that doesn't increment the tally
+
+//    |   |   |   |   | . |   |   |
+//    |   |   |   |   | . |   |   |
+//    |   |   |   | # | . |   |   |   
+//    |   |   |   |   | X |   |   |
+//    |   |   |   |   |   | # |   |
+//    |   |   |   |   |   |   |   |
+//    -----------------------------
+//    There is not a left diagonal victory, the tally is 0
+
+
+//    |   |   |   |   | . |   |   |
+//    |   |   |   |   | . |   |   |
+//    |   |   |   |   | . |   |   |
+//    |   |   | # | 1 | X | # |   |
+//    |   |   |   |   |   |   |   |
+//    |   |   |   |   |   |   |   |
+//    -----------------------------
+//    There is not a horizontal victory, the tally is 0
+
+    
+
+//    |   |   |   |   | . |   |   |
+//    |   |   |   |   | . |   |   |
+//    |   |   |   |   | . | 1 |   |
+//    |   |   |   |   | X |   |   |
+//    |   |   |   | 1 |   |   |   |
+//    |   |   | 2 |   |   |   |   |
+//    -----------------------------
+//    Since the total tally in this direction is 3,
+//our victory is confirmed 
